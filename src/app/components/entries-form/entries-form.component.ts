e596507d4a5e0629195b3d0ea2eeead3d3ca7738
Lucho <lsepu19@gmail.com>
2025-02-20 22:28:34 +0100

    Creación de componentes e interfaz de las entradas del blog

---
+++ src/app/components/entries-form/entries-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entries-form',
+  imports: [],
+  templateUrl: './entries-form.component.html',
+  styleUrl: './entries-form.component.css'
+})
+export class EntriesFormComponent {
+
+}
---
+++ src/app/components/entries-preview/entries-preview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entries-preview',
+  imports: [],
+  templateUrl: './entries-preview.component.html',
+  styleUrl: './entries-preview.component.css'
+})
+export class EntriesPreviewComponent {
+
+}
